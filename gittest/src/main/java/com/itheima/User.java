c48980398aa61d0a8d567680b4378fb6311c6b2e
wuwei <123096574@qq.com>
2019-01-28 17:55:23 +0800

    新建User

---
+++ gittest/src/main/java/com/itheima/User.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+/**
+ * @Author: fengxiaolou
+ * @Date: 2019-1-28 17:51
+ * @Description:
+ * @Modified by:
+ */
+public class User {
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
